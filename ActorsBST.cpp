6278456040e54bb88d13d6e29da3b8818835194b
brucetieu <brucetieu5@gmail.com>
2020-11-15 16:52:33 -0700

    Add insert()

--- ActorsBST.cpp
+++ ActorsBST.cpp
@@ -4,6 +4,11 @@
 
 #include "ActorsBST.h"
 
+/**
+ * Create a new node.
+ * @param actorsActresses The ActorsActresses object.
+ * @return A the newNode.
+ */
 BstNode* ActorsBST::getNewNode(ActorsActresses &actorsActresses) {
     BstNode* newNode = new BstNode();
 
@@ -17,3 +22,25 @@ BstNode* ActorsBST::getNewNode(ActorsActresses &actorsActresses) {
 
     return newNode;
 }
+
+BstNode* ActorsBST::insert(BstNode *root, ActorsActresses &actorsActresses) {
+    if(root == NULL) { // empty tree
+        root = getNewNode(actorsActresses);
+    }
+    // if data to be inserted is lesser, insert in left subtree.
+    else if(actorsActresses.name <= root->actorsActresses.name ||
+            actorsActresses.year <= root->actorsActresses.year ||
+            actorsActresses.getAward() <= root->actorsActresses.getAward() ||
+            actorsActresses.getWinner() <= root->actorsActresses.getWinner() ||
+            actorsActresses.getFilm() <= root->actorsActresses.getFilm()) {
+
+        root->left = insert(root->left, actorsActresses);
+    }
+        // else, insert in right subtree.
+    else {
+        root->right = insert(root->right, actorsActresses);
+    }
+    return root;
+}
+
+
